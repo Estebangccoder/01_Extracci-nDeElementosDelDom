36da56abf1bdc1c3ba00a03d0d9fae147344eaed
estebangccoder <estebangccoder@gmail.com>
2024-05-02 10:33:15 -0500

    Primeros pasos con DOM

---
+++ src/js/main.js
@@ -0,0 +1,28 @@
+let one =document.getElementById(id="btn-one")
+let two=document.getElementsByClassName("btn-two")
+let three =document.getElementsByName("btn-three")
+let four =document.getElementsByTagName("button")
+
+let five =document.querySelector("#btn-five")
+let six =document.querySelectorAll(".btn")
+let seven =document.querySelectorAll('[name = "btn-seven"]')
+let eight =document.querySelectorAll("button")
+
+// console.log(one)
+// console.log(two[0])
+// console.log(three[0])
+// console.log(four[3])
+// console.log(five)
+// console.log(six[5])
+// console.log(seven[0])
+// console.log(eight[7])
+
+let btnExample = document.getElementById("btn-example")
+
+
+console.log(btnExample)
+
+// btnExample.textContent="Hola mundo"
+// btnExample.addEventListener("click",function(){ 
+//     alert("Hola mundo")
+// })
